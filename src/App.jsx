630b1028e86e4774f1454d7277f3e01b8a7b019d
Amira Atef <amira.atef@bosta.co>
2018-11-14 10:41:07 +0200

    create homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import Home from './componets/Home';
+import Home from './components/Home';
 
 class App extends Component {
   componentDidMount() {
---
+++ src/components/Signup.jsx
@@ -0,0 +1,71 @@
+import React from 'react';
+import {
+  Select, Form, Input,
+} from 'antd';
+import PropTypes from 'prop-types';
+import classes from './styles.css';
+
+const FormItem = Form.Item;
+const { Option } = Select;
+
+class SignUpForm extends React.Component {
+  handleSubmit(e) {
+    e.preventDefault();
+    const { form } = this.props;
+    form.validateFields((err, values) => {
+      if (!err) {
+        console.log('Received values of form: ', values);
+      }
+    });
+  }
+
+  render() {
+    const { form } = this.props;
+    const { getFieldDecorator } = form;
+    return (
+      <div className={classes.signUpForm}>
+        <Form onSubmit={this.handleSubmit}>
+          <span className={classes.hDSignup}>Let’s Start Together!</span>
+
+          <FormItem>
+            {getFieldDecorator('Business Name', {
+              rules: [{ required: true, message: 'Please input your Business Name!' }],
+            })(
+              <Input className={classes.inpt} placeholder="Business Name" />,
+            )}
+          </FormItem>
+          <br />
+          <FormItem>
+            {getFieldDecorator('Business Category',
+              { rules: [{ required: true, message: 'Please input your Business Category!' }] })(
+                <Select defaultValue="Business Category" className={classes.dpdList}>
+                  <Option value="Usernamejack">Jack</Option>
+                  <Option value="Usernamelucy">Lucy</Option>
+                  <Option value="Usernamedisabled">Disabled</Option>
+                  <Option value="UsernameYiminghe">yiminghe</Option>
+                </Select>,
+            )}
+          </FormItem>
+          <br />
+
+          <FormItem>
+            {getFieldDecorator('Business Name', {
+              rules: [{ required: true, message: 'Please input your Business Name!' }],
+            })(
+              <Input className={classes.inpt} placeholder="Business Name" />,
+            )}
+
+          </FormItem>
+
+        </Form>
+      </div>
+    );
+  }
+}
+
+SignUpForm.propTypes = {
+  form: PropTypes.shape.isRequired,
+};
+const SignUp = Form.create()(SignUpForm);
+
+export default SignUp;
