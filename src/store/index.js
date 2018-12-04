fa1912e86c9f6b1c24867b0bd9bc07461da5f98a
Amira Atef <amira.atef@bosta.co>
2018-12-04 12:46:18 +0200

    add initial  Redux set up   deleted unused files

--- src/store/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux';
-import { reducer as formReducer } from 'redux-form';
-import appReducer from './reducers';
-
-export default combineReducers({
-  app: appReducer,
-  form: formReducer,
-
-});
